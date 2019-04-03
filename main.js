590a165ea57607cd940afed5087c678273c12efe
administrador <gilberto.tec@vivaldi.net>
2019-04-03 19:40:04 -0300

    implementando o pdf para exportações

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ let tray = null;/* Tray Icon = icone de de notificação no canto inferior direi
 app.on('ready',function(){
   main = new BrowserWindow({ //Janela Principal
       title:' MobiShare ',
-      width:500,
-      height:400,
+      width:900,
+      height:600,
       minWidth:500,
       minHeight:500,
       icon:path.join(__dirname,'images/icons/icon.png'),
@@ -36,7 +36,7 @@ app.on('ready',function(){
 
   Menu.setApplicationMenu(templates.genAppMenu(app));
 
-  //main.webContents.openDevTools()
+  main.webContents.openDevTools()
 
   //main.setOverlayIcon(path.join(__dirname,'images/icon.png'), "Hellow")
 })
@@ -48,8 +48,8 @@ ipcMain.on('openWindow',(event,page,data)=>{
   if(page in windows)windows[page] = null;// Destruindo janelas anteriormente criada
   /* Iniciando ou reiniciando Janela e armazenando a sua instancia nas windows */
   windows[page] = new BrowserWindow({
-    width:800,
-    heiht:600,
+    width:900,
+    height:700,
   })
   windows[page].loadURL(`file://${__dirname}/views/${page}`)
   windows[page].on('close',()=>windows[page]=null);
@@ -57,4 +57,26 @@ ipcMain.on('openWindow',(event,page,data)=>{
   windows[page].once('ready-to-show', () => {//Evita flash no carregamento da pagina
     windows[page].show()// Exibindo a pagina já carregada
   })
+})
+
+
+var fs = require('fs');
+var pdf = require('html-pdf');
+var options = { format: 'Letter' };
+
+/* Iniciando teste genPdf */
+ipcMain.on('genPDF',(event,pdfHtml)=>{
+  var evento = event;
+	console.log("chegou na geração do pdf");
+	pdf.create(pdfHtml.html, options)
+	.toFile('./'+pdfHtml.nome, function(err, caminho) {
+
+	  if(!caminho) return console.error(err);
+
+	  console.log("Caminho : ",caminho); // { filename: '/app/businesscard.pdf' }
+
+    evento.sender.send('genPdf-complite',caminho);
+
+	});
+
 })
--- views/renderer.js
+++ views/renderer.js
@@ -5,6 +5,16 @@ window.getWindow = (pagina) => {
   console.log("Chamando mainProcesse ");
   ipcRenderer.send('openWindow',pagina);
 }
+window.genPdf = (nome,html)=>{
+	console.log("CHamndo o mainProcess");
+	console.log(ipcRenderer.send('genPDF',{html,nome}));
+}
+
+/* Recebe o evento genpdf (do processo main) quando for conpletado */
+ipcRenderer.on('genPdf-complite',(event,arg)=>{
+	console.log("PDF gerado!!",arg);
+
+})
 
 
 document.addEventListener('DOMContentLoaded', () => {
